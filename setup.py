10f1b4b1b2538d1c46a0cefede448a605ece38fb
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2012-10-31 15:47:21 +0100

    Releasing version 0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='invenio-fabric',
-    version='0.1',
+    version='0.2',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=False,

4d9dc017cd4bf1019c55f7f2a9d146d0a7796c53
Lars Holm Nielsen <lars.holm.nielsen@cern.ch>
2012-11-15 10:22:51 +0100

    Add dependency on Sphinx to build docs.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     package_dir={'': 'src'},
     include_package_data=False,
     zip_safe=False,
-    install_requires=['distribute', 'Fabric>=1.4', 'Jinja2', 'virtualenvwrapper'],
+    install_requires=['distribute', 'Fabric>=1.4', 'Jinja2', 'virtualenvwrapper','Sphinx'],
     data_files=[
         ('share/atlantis/',[
             'examples/atlantis/fabfile.py',
